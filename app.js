7f40215ae57a1d97bdfe17939606a5a4cf59eba3
borisonr <borisonr@gmail.com>
2016-04-27 17:42:54 -0400

    swigging

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 var express = require( 'express' );
 var http = require('http');
 var fs = require('fs');
+var swig = require('swig');
+
 var app = express();
 var server = http.createServer();
 
+
 server.on('request', app);
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.use('/special/', function (req, res, next) {
+	console.log("you reached a special area");
+    next();
+})
+
+app.use(function (req, res, next) {
+	console.log(req.method, req.path, res.status().statusCode);
+    next();
+})
 
 app.get('/', function (req, res) {
     res.send('hello world');
 });
+
+app.get('/news', function (req, res) {
+    res.send('news');
+});
+
+swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+    console.log(output);
+});
+
 
 server.listen(3000, function () {
     console.log('Server listening');
---
+++ names.js
@@ -0,0 +1,14 @@
+var swig  = require('swig');
+
+var locals = {
+	description: 'This is a bare-bones template.',
+	title: 'An Example',
+	people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+    console.log(output);
+});
